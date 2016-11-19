00215f64b4357982a009c8815ebddb75cc6b55f7
Kaushik Prakash <k2.prakash@gmail.com>
2016-11-19 14:26:53 -0700

    Adding the right  MKL25Z4.h file for the project

--- Includes/MKL25Z4.h
+++ Includes/MKL25Z4.h
@@ -3876,6 +3876,222 @@ typedef struct {
  * @}
  */ /* end of group NV_Peripheral_Access_Layer */
 
+/* ----------------------------------------------------------------------------
+   -- NVIC
+   ---------------------------------------------------------------------------- */
+
+/**
+ * @addtogroup NVIC_Peripheral NVIC
+ * @{
+ */
+
+/** NVIC - Peripheral register structure */
+typedef struct NVIC_MemMap {
+  uint32_t ISER;                                   /**< Interrupt Set Enable Register, offset: 0x0 */
+  uint8_t RESERVED_0[124];
+  uint32_t ICER;                                   /**< Interrupt Clear Enable Register, offset: 0x80 */
+  uint8_t RESERVED_1[124];
+  uint32_t ISPR;                                   /**< Interrupt Set Pending Register, offset: 0x100 */
+  uint8_t RESERVED_2[124];
+  uint32_t ICPR;                                   /**< Interrupt Clear Pending Register, offset: 0x180 */
+  uint8_t RESERVED_3[380];
+  uint32_t IP[8];                                  /**< Interrupt Priority Register n, array offset: 0x300, array step: 0x4 */
+} volatile *NVIC_MemMapPtr;
+
+/* ----------------------------------------------------------------------------
+   -- NVIC - Register accessor macros
+   ---------------------------------------------------------------------------- */
+
+/**
+ * @addtogroup NVIC_Register_Accessor_Macros NVIC - Register accessor macros
+ * @{
+ */
+
+
+/* NVIC - Register accessors */
+#define NVIC_ISER_REG(base)                      ((base)->ISER)
+#define NVIC_ICER_REG(base)                      ((base)->ICER)
+#define NVIC_ISPR_REG(base)                      ((base)->ISPR)
+#define NVIC_ICPR_REG(base)                      ((base)->ICPR)
+#define NVIC_IP_REG(base,index)                  ((base)->IP[index])
+
+/**
+ * @}
+ */ /* end of group NVIC_Register_Accessor_Macros */
+
+
+/* ----------------------------------------------------------------------------
+   -- NVIC Register Masks
+   ---------------------------------------------------------------------------- */
+
+/**
+ * @addtogroup NVIC_Register_Masks NVIC Register Masks
+ * @{
+ */
+
+/* ISER Bit Fields */
+#define NVIC_ISER_SETENA_MASK                    0xFFFFFFFFu
+#define NVIC_ISER_SETENA_SHIFT                   0
+#define NVIC_ISER_SETENA(x)                      (((uint32_t)(((uint32_t)(x))<<NVIC_ISER_SETENA_SHIFT))&NVIC_ISER_SETENA_MASK)
+/* ICER Bit Fields */
+#define NVIC_ICER_CLRENA_MASK                    0xFFFFFFFFu
+#define NVIC_ICER_CLRENA_SHIFT                   0
+#define NVIC_ICER_CLRENA(x)                      (((uint32_t)(((uint32_t)(x))<<NVIC_ICER_CLRENA_SHIFT))&NVIC_ICER_CLRENA_MASK)
+/* ISPR Bit Fields */
+#define NVIC_ISPR_SETPEND_MASK                   0xFFFFFFFFu
+#define NVIC_ISPR_SETPEND_SHIFT                  0
+#define NVIC_ISPR_SETPEND(x)                     (((uint32_t)(((uint32_t)(x))<<NVIC_ISPR_SETPEND_SHIFT))&NVIC_ISPR_SETPEND_MASK)
+/* ICPR Bit Fields */
+#define NVIC_ICPR_CLRPEND_MASK                   0xFFFFFFFFu
+#define NVIC_ICPR_CLRPEND_SHIFT                  0
+#define NVIC_ICPR_CLRPEND(x)                     (((uint32_t)(((uint32_t)(x))<<NVIC_ICPR_CLRPEND_SHIFT))&NVIC_ICPR_CLRPEND_MASK)
+/* IP Bit Fields */
+#define NVIC_IP_PRI_0_MASK                       0xFFu
+#define NVIC_IP_PRI_0_SHIFT                      0
+#define NVIC_IP_PRI_0(x)                         (((uint32_t)(((uint32_t)(x))<<NVIC_IP_PRI_0_SHIFT))&NVIC_IP_PRI_0_MASK)
+#define NVIC_IP_PRI_28_MASK                      0xFFu
+#define NVIC_IP_PRI_28_SHIFT                     0
+#define NVIC_IP_PRI_28(x)                        (((uint32_t)(((uint32_t)(x))<<NVIC_IP_PRI_28_SHIFT))&NVIC_IP_PRI_28_MASK)
+#define NVIC_IP_PRI_24_MASK                      0xFFu
+#define NVIC_IP_PRI_24_SHIFT                     0
+#define NVIC_IP_PRI_24(x)                        (((uint32_t)(((uint32_t)(x))<<NVIC_IP_PRI_24_SHIFT))&NVIC_IP_PRI_24_MASK)
+#define NVIC_IP_PRI_20_MASK                      0xFFu
+#define NVIC_IP_PRI_20_SHIFT                     0
+#define NVIC_IP_PRI_20(x)                        (((uint32_t)(((uint32_t)(x))<<NVIC_IP_PRI_20_SHIFT))&NVIC_IP_PRI_20_MASK)
+#define NVIC_IP_PRI_4_MASK                       0xFFu
+#define NVIC_IP_PRI_4_SHIFT                      0
+#define NVIC_IP_PRI_4(x)                         (((uint32_t)(((uint32_t)(x))<<NVIC_IP_PRI_4_SHIFT))&NVIC_IP_PRI_4_MASK)
+#define NVIC_IP_PRI_16_MASK                      0xFFu
+#define NVIC_IP_PRI_16_SHIFT                     0
+#define NVIC_IP_PRI_16(x)                        (((uint32_t)(((uint32_t)(x))<<NVIC_IP_PRI_16_SHIFT))&NVIC_IP_PRI_16_MASK)
+#define NVIC_IP_PRI_12_MASK                      0xFFu
+#define NVIC_IP_PRI_12_SHIFT                     0
+#define NVIC_IP_PRI_12(x)                        (((uint32_t)(((uint32_t)(x))<<NVIC_IP_PRI_12_SHIFT))&NVIC_IP_PRI_12_MASK)
+#define NVIC_IP_PRI_8_MASK                       0xFFu
+#define NVIC_IP_PRI_8_SHIFT                      0
+#define NVIC_IP_PRI_8(x)                         (((uint32_t)(((uint32_t)(x))<<NVIC_IP_PRI_8_SHIFT))&NVIC_IP_PRI_8_MASK)
+#define NVIC_IP_PRI_13_MASK                      0xFF00u
+#define NVIC_IP_PRI_13_SHIFT                     8
+#define NVIC_IP_PRI_13(x)                        (((uint32_t)(((uint32_t)(x))<<NVIC_IP_PRI_13_SHIFT))&NVIC_IP_PRI_13_MASK)
+#define NVIC_IP_PRI_21_MASK                      0xFF00u
+#define NVIC_IP_PRI_21_SHIFT                     8
+#define NVIC_IP_PRI_21(x)                        (((uint32_t)(((uint32_t)(x))<<NVIC_IP_PRI_21_SHIFT))&NVIC_IP_PRI_21_MASK)
+#define NVIC_IP_PRI_29_MASK                      0xFF00u
+#define NVIC_IP_PRI_29_SHIFT                     8
+#define NVIC_IP_PRI_29(x)                        (((uint32_t)(((uint32_t)(x))<<NVIC_IP_PRI_29_SHIFT))&NVIC_IP_PRI_29_MASK)
+#define NVIC_IP_PRI_1_MASK                       0xFF00u
+#define NVIC_IP_PRI_1_SHIFT                      8
+#define NVIC_IP_PRI_1(x)                         (((uint32_t)(((uint32_t)(x))<<NVIC_IP_PRI_1_SHIFT))&NVIC_IP_PRI_1_MASK)
+#define NVIC_IP_PRI_9_MASK                       0xFF00u
+#define NVIC_IP_PRI_9_SHIFT                      8
+#define NVIC_IP_PRI_9(x)                         (((uint32_t)(((uint32_t)(x))<<NVIC_IP_PRI_9_SHIFT))&NVIC_IP_PRI_9_MASK)
+#define NVIC_IP_PRI_17_MASK                      0xFF00u
+#define NVIC_IP_PRI_17_SHIFT                     8
+#define NVIC_IP_PRI_17(x)                        (((uint32_t)(((uint32_t)(x))<<NVIC_IP_PRI_17_SHIFT))&NVIC_IP_PRI_17_MASK)
+#define NVIC_IP_PRI_25_MASK                      0xFF00u
+#define NVIC_IP_PRI_25_SHIFT                     8
+#define NVIC_IP_PRI_25(x)                        (((uint32_t)(((uint32_t)(x))<<NVIC_IP_PRI_25_SHIFT))&NVIC_IP_PRI_25_MASK)
+#define NVIC_IP_PRI_5_MASK                       0xFF00u
+#define NVIC_IP_PRI_5_SHIFT                      8
+#define NVIC_IP_PRI_5(x)                         (((uint32_t)(((uint32_t)(x))<<NVIC_IP_PRI_5_SHIFT))&NVIC_IP_PRI_5_MASK)
+#define NVIC_IP_PRI_2_MASK                       0xFF0000u
+#define NVIC_IP_PRI_2_SHIFT                      16
+#define NVIC_IP_PRI_2(x)                         (((uint32_t)(((uint32_t)(x))<<NVIC_IP_PRI_2_SHIFT))&NVIC_IP_PRI_2_MASK)
+#define NVIC_IP_PRI_26_MASK                      0xFF0000u
+#define NVIC_IP_PRI_26_SHIFT                     16
+#define NVIC_IP_PRI_26(x)                        (((uint32_t)(((uint32_t)(x))<<NVIC_IP_PRI_26_SHIFT))&NVIC_IP_PRI_26_MASK)
+#define NVIC_IP_PRI_18_MASK                      0xFF0000u
+#define NVIC_IP_PRI_18_SHIFT                     16
+#define NVIC_IP_PRI_18(x)                        (((uint32_t)(((uint32_t)(x))<<NVIC_IP_PRI_18_SHIFT))&NVIC_IP_PRI_18_MASK)
+#define NVIC_IP_PRI_14_MASK                      0xFF0000u
+#define NVIC_IP_PRI_14_SHIFT                     16
+#define NVIC_IP_PRI_14(x)                        (((uint32_t)(((uint32_t)(x))<<NVIC_IP_PRI_14_SHIFT))&NVIC_IP_PRI_14_MASK)
+#define NVIC_IP_PRI_6_MASK                       0xFF0000u
+#define NVIC_IP_PRI_6_SHIFT                      16
+#define NVIC_IP_PRI_6(x)                         (((uint32_t)(((uint32_t)(x))<<NVIC_IP_PRI_6_SHIFT))&NVIC_IP_PRI_6_MASK)
+#define NVIC_IP_PRI_30_MASK                      0xFF0000u
+#define NVIC_IP_PRI_30_SHIFT                     16
+#define NVIC_IP_PRI_30(x)                        (((uint32_t)(((uint32_t)(x))<<NVIC_IP_PRI_30_SHIFT))&NVIC_IP_PRI_30_MASK)
+#define NVIC_IP_PRI_22_MASK                      0xFF0000u
+#define NVIC_IP_PRI_22_SHIFT                     16
+#define NVIC_IP_PRI_22(x)                        (((uint32_t)(((uint32_t)(x))<<NVIC_IP_PRI_22_SHIFT))&NVIC_IP_PRI_22_MASK)
+#define NVIC_IP_PRI_10_MASK                      0xFF0000u
+#define NVIC_IP_PRI_10_SHIFT                     16
+#define NVIC_IP_PRI_10(x)                        (((uint32_t)(((uint32_t)(x))<<NVIC_IP_PRI_10_SHIFT))&NVIC_IP_PRI_10_MASK)
+#define NVIC_IP_PRI_31_MASK                      0xFF000000u
+#define NVIC_IP_PRI_31_SHIFT                     24
+#define NVIC_IP_PRI_31(x)                        (((uint32_t)(((uint32_t)(x))<<NVIC_IP_PRI_31_SHIFT))&NVIC_IP_PRI_31_MASK)
+#define NVIC_IP_PRI_27_MASK                      0xFF000000u
+#define NVIC_IP_PRI_27_SHIFT                     24
+#define NVIC_IP_PRI_27(x)                        (((uint32_t)(((uint32_t)(x))<<NVIC_IP_PRI_27_SHIFT))&NVIC_IP_PRI_27_MASK)
+#define NVIC_IP_PRI_23_MASK                      0xFF000000u
+#define NVIC_IP_PRI_23_SHIFT                     24
+#define NVIC_IP_PRI_23(x)                        (((uint32_t)(((uint32_t)(x))<<NVIC_IP_PRI_23_SHIFT))&NVIC_IP_PRI_23_MASK)
+#define NVIC_IP_PRI_3_MASK                       0xFF000000u
+#define NVIC_IP_PRI_3_SHIFT                      24
+#define NVIC_IP_PRI_3(x)                         (((uint32_t)(((uint32_t)(x))<<NVIC_IP_PRI_3_SHIFT))&NVIC_IP_PRI_3_MASK)
+#define NVIC_IP_PRI_19_MASK                      0xFF000000u
+#define NVIC_IP_PRI_19_SHIFT                     24
+#define NVIC_IP_PRI_19(x)                        (((uint32_t)(((uint32_t)(x))<<NVIC_IP_PRI_19_SHIFT))&NVIC_IP_PRI_19_MASK)
+#define NVIC_IP_PRI_15_MASK                      0xFF000000u
+#define NVIC_IP_PRI_15_SHIFT                     24
+#define NVIC_IP_PRI_15(x)                        (((uint32_t)(((uint32_t)(x))<<NVIC_IP_PRI_15_SHIFT))&NVIC_IP_PRI_15_MASK)
+#define NVIC_IP_PRI_11_MASK                      0xFF000000u
+#define NVIC_IP_PRI_11_SHIFT                     24
+#define NVIC_IP_PRI_11(x)                        (((uint32_t)(((uint32_t)(x))<<NVIC_IP_PRI_11_SHIFT))&NVIC_IP_PRI_11_MASK)
+#define NVIC_IP_PRI_7_MASK                       0xFF000000u
+#define NVIC_IP_PRI_7_SHIFT                      24
+#define NVIC_IP_PRI_7(x)                         (((uint32_t)(((uint32_t)(x))<<NVIC_IP_PRI_7_SHIFT))&NVIC_IP_PRI_7_MASK)
+
+/**
+ * @}
+ */ /* end of group NVIC_Register_Masks */
+
+
+/* NVIC - Peripheral instance base addresses */
+/** Peripheral NVIC base pointer */
+#define NVIC_BASE_PTR                            ((NVIC_MemMapPtr)0xE000E100u)
+/** Array initializer of NVIC peripheral base pointers */
+#define NVIC_BASE_PTRS                           { NVIC_BASE_PTR }
+
+/* ----------------------------------------------------------------------------
+   -- NVIC - Register accessor macros
+   ---------------------------------------------------------------------------- */
+
+/**
+ * @addtogroup NVIC_Register_Accessor_Macros NVIC - Register accessor macros
+ * @{
+ */
+
+
+/* NVIC - Register instance definitions */
+/* NVIC */
+#define NVIC_ISER                                NVIC_ISER_REG(NVIC_BASE_PTR)
+#define NVIC_ICER                                NVIC_ICER_REG(NVIC_BASE_PTR)
+#define NVIC_ISPR                                NVIC_ISPR_REG(NVIC_BASE_PTR)
+#define NVIC_ICPR                                NVIC_ICPR_REG(NVIC_BASE_PTR)
+#define NVIC_IPR0                                NVIC_IP_REG(NVIC_BASE_PTR,0)
+#define NVIC_IPR1                                NVIC_IP_REG(NVIC_BASE_PTR,1)
+#define NVIC_IPR2                                NVIC_IP_REG(NVIC_BASE_PTR,2)
+#define NVIC_IPR3                                NVIC_IP_REG(NVIC_BASE_PTR,3)
+#define NVIC_IPR4                                NVIC_IP_REG(NVIC_BASE_PTR,4)
+#define NVIC_IPR5                                NVIC_IP_REG(NVIC_BASE_PTR,5)
+#define NVIC_IPR6                                NVIC_IP_REG(NVIC_BASE_PTR,6)
+#define NVIC_IPR7                                NVIC_IP_REG(NVIC_BASE_PTR,7)
+
+/* NVIC - Register array accessors */
+#define NVIC_IP(index)                           NVIC_IP_REG(NVIC_BASE_PTR,index)
+
+/**
+ * @}
+ */ /* end of group NVIC_Register_Accessor_Macros */
+
+
+/**
+ * @}
+ */ /* end of group NVIC_Peripheral */
+
+
 
 /* ----------------------------------------------------------------------------
    -- OSC Peripheral Access Layer
